0169221b088fdc1b1f7d68037f68a010c30106d5
Vlad Meh <vladmeh@gmail.com>
2014-05-27 16:59:08 +0400

    vladmeh dendor

--- js/contact.js
+++ js/contact.js
@@ -4,38 +4,37 @@
 
   function init() {
     var mapOptions = {
-      center: new google.maps.LatLng(59.951811,30.213128),
+      center: new google.maps.LatLng(59.951811,30.215228),
       zoom: 16,
       zoomControl: true,
       zoomControlOptions: {
-          style: google.maps.ZoomControlStyle.DEFAULT,
+          style: google.maps.ZoomControlStyle.SMALL,
       },
       disableDoubleClickZoom: true,
       mapTypeControl: true,
       mapTypeControlOptions: {
-          style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+          style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
       },
-      scaleControl: true,
-      scrollwheel: true,
-      streetViewControl: true,
+      scaleControl: false,
+      scrollwheel: false,
+      streetViewControl: false,
       draggable : true,
-      overviewMapControl: true,
-      overviewMapControlOptions: {
-          opened: false,
-      },
+      overviewMapControl: false,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: [{featureType:"administrative",stylers:[{visibility:"off"}]},{featureType:"poi",stylers:[{visibility:"simplified"}]},{featureType:"road",stylers:[{visibility:"simplified"}]},{featureType:"water",
+      stylers:[{visibility:"simplified"}]},{featureType:"transit",stylers:[{visibility:"simplified"}]},{featureType:"landscape",stylers:[{visibility:"simplified"}]},{featureType:"road.highway",stylers:[{visibility:"off"}]},{featureType:"road.local",stylers:[{visibility:"on"}]},{featureType:"road.highway",elementType:"geometry",stylers:[{visibility:"on"}]},{featureType:"water",stylers:[{color:"#84afa3"},{lightness:52}]},{stylers:[{saturation:-77}]},{featureType:"road"}],
     }
 
     var mapElement = document.getElementById('map');
     var map = new google.maps.Map(mapElement, mapOptions);
     var locations = [
-      ['улица Кораблестроителей', 59.94875479999999, 30.216112]
+      [59.951876,30.215965]
     ];
 
     for (i = 0; i < locations.length; i++) {
       marker = new google.maps.Marker({
         icon: '',
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        position: new google.maps.LatLng(locations[i][0], locations[i][1]),
         map: map
       });
     }
