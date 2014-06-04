f726432b228029187e451e1effa7646ac2f4faaa
Vlad Meh <vladmeh@gmail.com>
2014-06-04 16:56:31 +0400

    vladmeh dendor

--- js/script.js
+++
@@ -1,8 +0,0 @@
-(function($){
-  /*var hWrap = $('.wrapper').height(),
-      hBody = $(document).height()-$('.footer').height();
-
-  if (hWrap < hBody){
-    $('.wrapper').height(hBody);
-  }*/
-})(jQuery);
