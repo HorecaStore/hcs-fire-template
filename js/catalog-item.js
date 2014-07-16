0f9482e4c6371ac4993f248ea8fc97e803e08e96
Vlad Meh <vladmeh@gmail.com>
2014-07-16 12:36:06 +0400

    vladmeh dendor

--- js/catalog-item.js
+++ js/catalog-item.js
@@ -17,4 +17,24 @@ head.ready('mfp', function(){
     type: 'inline',
     preloader: false
   });
+  $('.open-mfp-gallery').on('click', function () {
+    $('.mfp-img-gallery').magnificPopup('open');
+});
+  $('.mfp-img-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title');
+      }
+    }
+  });
 });
